d2002046c0600cd31f73b8709a8e2aa15228fc2a
lucasfeva <luscas.png@gmail.com>
2025-07-19 03:12:48 -0300

    feat: enhance TechnologyCarousel with hover effects and add technology names

--- src/components/technology-carousel.tsx
+++ src/components/technology-carousel.tsx
@@ -22,13 +22,18 @@ export function TechnologyCarousel() {
         {Technologies.map((tech) => (
           <CarouselItem key={tech.icon} className="md:basis-1/3 lg:basis-1/5">
             <div
-              className="flex aspect-square items-center justify-center px-6 py-24 rounded-lg shadow-lg"
+              className="relative group flex aspect-square items-center justify-center px-6 py-24 rounded-lg shadow-lg overflow-hidden cursor-pointer transition-all duration-300"
               style={{
                 background: `linear-gradient(135deg, ${tech.color1}, ${tech.color2})`,
               }}
             >
-              <div className="relative w-16 h-16">
-                <Image src={tech.icon} fill alt="Ícone de tecnologia" />
+              <div className="relative w-16 h-16 transition-all duration-300 group-hover:brightness-50 group-hover:scale-90">
+                <Image src={tech.icon} fill alt={`${tech.name}`} />
+              </div>
+              <div className="absolute inset-0 bg-neutral-950/70 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                <h3 className="text-secondary dark:text-foreground font-semibold text-lg text-center px-2">
+                  {tech.name}
+                </h3>
               </div>
             </div>
           </CarouselItem>
--- src/constants/technology.ts
+++ src/constants/technology.ts
@@ -1,80 +1,90 @@
 interface Technologies {
+  name: string;
   icon: string;
   color1: string;
   color2: string;
 }
 export const Technologies: Technologies[] = [
   {
-    icon: "/icons/ts.svg",
-    color1: "#377cc8",
-    color2: "#42c3fa",
-  },
-  {
-    icon: "/icons/js.svg",
-    color1: "#fed650",
-    color2: "#feb250",
-  },
-  {
-    icon: "/icons/html.svg",
-    color1: "#e34f26",
-    color2: "#eb6427",
-  },
-  {
-    icon: "/icons/css.svg",
-    color1: "#0975bd",
-    color2: "#32abe1",
-  },
-  {
+    name: "React",
     icon: "/icons/react.svg",
     color1: "#087ea4",
     color2: "#0bb8f0",
   },
   {
+    name: "Next.js",
     icon: "/icons/nextjs.svg",
     color1: "#000000",
     color2: "#222222",
   },
   {
+    name: "Tailwind CSS",
     icon: "/icons/tailwind.svg",
     color1: "#63c8b9",
     color2: "#3e91ad",
   },
   {
+    name: "Prisma",
     icon: "/icons/prisma.svg",
     color1: "#5f6cd9",
     color2: "#77d89a",
   },
   {
+    name: "Drizzle ORM",
+    icon: "/icons/drizzle.svg",
+    color1: "#c5f74f",
+    color2: "#7c9c32",
+  },
+  {
+    name: "Node.js",
     icon: "/icons/node.svg",
     color1: "#41873f",
     color2: "#68bb49",
   },
   {
+    name: "PHP",
     icon: "/icons/php.svg",
     color1: "#4d588e",
     color2: "#616fb4",
   },
   {
+    name: "Laravel",
     icon: "/icons/laravel.svg",
     color1: "#c2180f",
     color2: "#FF2D20",
   },
   {
+    name: "MySQL",
     icon: "/icons/mysql.svg",
     color1: "#085466",
     color2: "#e99208",
   },
   {
+    name: "HTML",
+    icon: "/icons/html.svg",
+    color1: "#e34f26",
+    color2: "#eb6427",
+  },
+  {
+    name: "CSS",
+    icon: "/icons/css.svg",
+    color1: "#0975bd",
+    color2: "#32abe1",
+  },
+  {
+    name: "Git",
     icon: "/icons/git.svg",
     color1: "#ce240d",
     color2: "#EE513B",
   },
   {
+    name: "Docker",
     icon: "/icons/docker.svg",
     color1: "#147ac8",
     color2: "#2396ED",
   },
   {
+    name: "Cypress",
     icon: "/icons/cypress.svg",
     color1: "#397665",
     color2: "#69d3a7",
